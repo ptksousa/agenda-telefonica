2b4410aee01677ae9ef8430277231b38743a8f8d
Patrick <patrickdesousabr@gmail.com>
2025-11-26 20:55:22 -0300

    0.1

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
-from funcoes.funcoes import menu
+from funcoes.funcoes import menu, cadastrar_contato
+
 
 agenda = []
 
-escolha = menu()
+while True:
+    chave = menu()
 
-if escolha == 1:
-    while True: 
-        contato = []
-        nome = input('Digíte o nome: ')
-        numero = int(input('Digíte o número de telefone: '))
-        contato.append(nome)
-        contato.append(numero)
-        agenda.append(contato[:])
+    if chave == 1:
+        cadastrar_contato(agenda)
+
+    elif chave == 2:
+        print('-='*20)
+        print('0 - RETORNAR AO MENU')
+        for i, f in enumerate(agenda):
+            print(f'{i+1} - Nome: {f[0]}| Número: {f[1]}')
+        remover = int(input('Escolha o índice: '))
+        if remover == 0: 
+            print()
+        else:
+            del agenda[remover-1]
+    
+    elif chave == 3:
+        print('-='*20)
+        for i, f in enumerate(agenda):
+            print(f'{i+1} - Nome: {f[0]}| Número: {f[1]}')
+        input('Pressione ENTER.')
+    
+    elif chave == 4:
         break
+    
+    else:
+        input('Escolha números existentes nos índices do menu. (PRESSIONE ENTER)')
