13c9df0bc054928f0f6c27a4cfa2da65234add3f
Patrick <patrickdesousabr@gmail.com>
2025-11-27 16:07:09 -0300

    Tratamento de exceções nas funções

--- funcoes/funcoes.py
+++ funcoes/funcoes.py
@@ -7,14 +7,26 @@ def menu():
     print('[ 3 ] VER AGENDA')
     print('[ 4 ] SAIR')
 
-    chave = int(input('Escolha uma opção: '))
-    return chave
+    while True:
+        try:    
+            chave = int(input('Escolha uma opção: '))
+            if chave < 5 and chave > 0:
+                return chave
+            else:
+                print('Escolha apenas números presentes nos índices do menu.')
+        except:
+            print('Digíte apenas números presentes no índice do menu.')
 
 
 def cadastrar_contato(lista):
     print('-='*20)
     nome = input('Digíte o nome: ').upper()
-    numero = int(input('Digíte o número: '))
+    while True:
+        try:
+            numero = int(input('Digíte o número: '))
+            break
+        except:
+            print('Dígite um número de telefone válido')
     contato = []
     contato.append(nome)
     contato.append(numero)
